f0d70727df5a222888434b9f78fd9c4e2fd42f86
pixls <tha.pixls@gmail.com>
2022-01-06 19:18:33 -0500

    moving everything into a class

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -3,31 +3,42 @@ console.log("Let's get this party started!");
 const searchForm = document.getElementById("id");
 const giphyAPIKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
 
-const newSearch = (q, api_key) => {
-    return params = {
+class gifSearch {
+    constructor (q, api_key) {
         q,
         api_key
     }
+    async findGif(searchParams) {
+        const gifSearch = await axios.get("https://api.giphy.com/v1/gifs/search", {params : searchParams})
+        const gifNumber = Math.floor(Math.random()*50)
+        let result = gifSearch.data.data
+        // console.log(result);
+        return result
+    }
+    async getRandomGif (searchTerm) {
+        const result = await findGif(this.newSearch(searchTerm, giphyAPIKey));
+        gifNumber = Math.floor(Math.random()*50)
+        // return result[gifNumber].embed_url
+        console.log(result[gifNumber].embed_url)
+    }
+    addNewGif (searchResults) {
+        const gifArea = document.getElementById("gifArea");
+        const newGif = document.createElement("div");
+        newGif.classList.add("column card")
+        newGif.innerHTML = `<img class="image" src="searchResults"`
+    }
 }
 
-async function findGif(searchParams) {
-    const gifSearch = await axios.get("https://api.giphy.com/v1/gifs/search", {params : searchParams})
-    const gifNumber = Math.floor(Math.random()*50)
-    let result = gifSearch.data.data
-    // console.log(result);
-    return result
-}
+// const newSearch = (q, api_key) => {
+//     return params = {
+//         q,
+//         api_key
+//     }
+// }
 
-async function getRandomGif (searchTerm) {
-    const result = await findGif(newSearch(searchTerm, giphyAPIKey));
-    gifNumber = Math.floor(Math.random()*50)
-    // return result[gifNumber].embed_url
-    console.log(result[gifNumber].embed_url)
-}
+new gifSearch ("banana", giphyAPIKey)
 
-function addNewGif (searchResults) {
-    const gifArea = document.getElementById("gifArea");
-    const newGif = document.createElement("div");
-    newGif.classList.add("column card")
-    newGif.innerHTML = `<img class="image" src="searchResults"`
-}
+
+
+
+
