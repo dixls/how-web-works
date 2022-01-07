f301be9a7e4a511c6e23a9f65a8e046adb08d1aa
pixls <tha.pixls@gmail.com>
2022-01-06 19:18:10 -0500

    web page layout, beginner functions

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,1 +1,33 @@
 console.log("Let's get this party started!");
+
+const searchForm = document.getElementById("id");
+const giphyAPIKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+
+const newSearch = (q, api_key) => {
+    return params = {
+        q,
+        api_key
+    }
+}
+
+async function findGif(searchParams) {
+    const gifSearch = await axios.get("https://api.giphy.com/v1/gifs/search", {params : searchParams})
+    const gifNumber = Math.floor(Math.random()*50)
+    let result = gifSearch.data.data
+    // console.log(result);
+    return result
+}
+
+async function getRandomGif (searchTerm) {
+    const result = await findGif(newSearch(searchTerm, giphyAPIKey));
+    gifNumber = Math.floor(Math.random()*50)
+    // return result[gifNumber].embed_url
+    console.log(result[gifNumber].embed_url)
+}
+
+function addNewGif (searchResults) {
+    const gifArea = document.getElementById("gifArea");
+    const newGif = document.createElement("div");
+    newGif.classList.add("column card")
+    newGif.innerHTML = `<img class="image" src="searchResults"`
+}
